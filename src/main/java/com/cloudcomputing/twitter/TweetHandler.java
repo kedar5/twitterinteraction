8be887ebb8599a1392a692aa110ac88ed9f4cdfb
Kedar Deshpande <kedardeshpande9@gmail.com>
2022-11-29 20:04:59 -0500

    rds connectivity changes

--- src/main/java/com/cloudcomputing/twitter/TweetHandler.java
+++ src/main/java/com/cloudcomputing/twitter/TweetHandler.java
@@ -28,7 +28,7 @@ public class TweetHandler {
     String values = (user_id +","+ type+","+phrase+","+hashtag);
     System.out.println(values);
     client
-      .query("SELECT * FROM Data WHERE id2 = 582276888")
+      .query("SELECT * FROM Data WHERE uid2 = 582276888")
       .execute(ar -> {
         if (ar.succeeded()) {
           RowSet<Row> result = ar.result();
